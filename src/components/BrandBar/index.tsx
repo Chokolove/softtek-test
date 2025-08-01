333d187e7b713dc998cc7d0c10dc19d786639464
Carlos LL <carlosll2493@gmail.com>
2025-08-01 18:36:39 -0500

    Add responsive to brandbar

--- src/components/BrandBar/index.tsx
+++ src/components/BrandBar/index.tsx
@@ -6,7 +6,9 @@ export default function BrandBar() {
     <div className="brand-bar">
       <img className="brand-bar__logo" src="/rimacLogo.png" alt="logo" />
       <div className="brand-bar__contact">
-        <p className="brand-bar__text">¡Compra por este medio!</p>
+        <p className="brand-bar__text brand-bar__text--mobile-hidden">
+          ¡Compra por este medio!
+        </p>
         <p className="brand-bar__text brand-bar__text--phone">
           <Phone width={16} height={16} fill="#03050f" stroke="#03050f" />
           (01) 411 6001
