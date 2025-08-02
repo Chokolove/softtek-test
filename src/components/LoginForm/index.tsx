adf72dc8a7b5486f034eb10af195284a8218e193
Carlos LL <carlosll2493@gmail.com>
2025-08-02 03:07:30 -0500

    Add validation and user state

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,42 +1,51 @@
 import { useForm } from "react-hook-form";
 import "./loginForm.scss";
-import toast, { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { login } from "@/redux/userSlice";
+
+type FormValues = {
+  docType: string;
+  docNumber: string;
+  phone: string;
+  terms1: boolean;
+  terms2: boolean;
+};
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormValues>();
+  const dispatch = useDispatch();
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = (data: FormValues) => {
+    dispatch(
+      login({
+        docType: data.docType,
+        docNumber: data.docNumber,
+        phone: data.phone,
+      })
+    );
   };
 
-  const onError = (errors: any) => {
-    Object.values(errors).forEach((err: any) => {
-      if (err?.message) toast.error(err.message);
-    });
-  };
   return (
     <div>
-      <Toaster
-        position="top-center"
-        containerStyle={{
-          marginTop: "8rem",
-        }}
-      />
       <p className="login-form__text">
         Tú eliges cuánto pagar. Ingresa tus datos, cotiza y recibe nuestra
         asesoría. 100% online.
       </p>
-      <form className="login-form" onSubmit={handleSubmit(onSubmit, onError)}>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="login-form__subgroup">
           <div className="login-form__field login-form__field--doc">
             <div className="login-form__doc-wrapper">
               <select
                 className="login-form__select"
-                {...register("docType", { required: true })}
+                {...register("docType", {
+                  required: true,
+                  validate: (value) =>
+                    value === "dni" || "Solo se permite DNI por ahora",
+                })}
               >
                 <option value="dni">DNI</option>
                 <option value="ce">CE</option>
@@ -56,9 +65,14 @@ export default function LoginForm() {
                   type="text"
                   placeholder=" "
                   {...register("docNumber", {
-                    required: "Ingresa tu número de documento",
+                    required: "Nro. de Documento requerido",
                   })}
                 />
+                {errors.docNumber && (
+                  <span className="login-form__error">
+                    {errors.docNumber.message}
+                  </span>
+                )}
               </div>
             </div>
           </div>
@@ -74,13 +88,18 @@ export default function LoginForm() {
                 type="tel"
                 placeholder=" "
                 {...register("phone", {
-                  required: "Ingresa tu número de celular",
+                  required: "Numero de celular requerido",
                   pattern: {
                     value: /^[0-9]{9}$/,
                     message: "Debe tener 9 dígitos",
                   },
                 })}
               />
+              {errors.phone && (
+                <span className="login-form__error">
+                  {errors.phone.message}
+                </span>
+              )}
             </div>
           </div>
         </div>
@@ -90,11 +109,16 @@ export default function LoginForm() {
               <input
                 type="checkbox"
                 className="login-form__checkbox"
-                {...register("terms1", { required: "Error box 1" })}
+                {...register("terms1", {
+                  required: "Ambas politicas deben ser aceptadas",
+                })}
               />
               <span className="login-form__custom-box" />
               Acepto lo Política de Privacidad
             </label>
+            {errors.terms1 && (
+              <span className="login-form__error">{errors.terms1.message}</span>
+            )}
           </div>
 
           <div className=" login-form__field--checkbox">
@@ -102,11 +126,16 @@ export default function LoginForm() {
               <input
                 type="checkbox"
                 className="login-form__checkbox"
-                {...register("terms2", { required: "Error box 2" })}
+                {...register("terms2", {
+                  required: "Ambas politicas deben ser aceptadas",
+                })}
               />
               <span className="login-form__custom-box" />
               Acepto la Política Comunicaciones Comerciales
             </label>
+            {errors.terms2 && (
+              <span className="login-form__error">{errors.terms2.message}</span>
+            )}
           </div>
 
           <a
