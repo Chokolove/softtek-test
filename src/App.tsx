22b82c8bdf627af0839e8a4b95382ee58b8e5846
Carlos LL <carlosll2493@gmail.com>
2025-08-04 00:46:11 -0500

    Add vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Plans from "./pages/Plans";
@@ -7,8 +7,9 @@ import Summary from "./pages/Summary";
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<ProtectedRoute />}>
+      <Route element={<ProtectedRoute />}>
         <Route path="/plans" element={<Plans />} />
         <Route path="/summary" element={<Summary />} />
       </Route>
